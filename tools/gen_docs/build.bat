b79c3fcc238df6dac0ef18f20022ccca0f083764
Mykola Morozov <mcmikecreations@gmail.com>
2021-12-09 14:38:46 +0100

    Added docs initial setup.
    Signed-off-by: Mykola Morozov <mcmikecreations@gmail.com>

---
+++ tools/gen_docs/build.bat
@@ -0,0 +1,2 @@
+:: This needs to be run from the root svitogen directory.
+python -m mkdocs build
---
+++ tools/gen_docs/setup.bat
@@ -0,0 +1,5 @@
+:: This needs to be run from the root svitogen directory.
+pip install mkdocs
+pip install mkdocs-minify-plugin
+pip install mkdocs-redirects
+pip install mkdocs-material
